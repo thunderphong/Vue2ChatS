b84ac874d7c3e15ce515fabd629f359164bdaa86
thunderphong <thunderthunderphong@gmail.com>
2020-10-22 18:43:15 +0700

    second

--- index.js
+++ index.js
@@ -17,11 +17,36 @@ app.use(express.urlencoded({ extended: true }));
 const cors = require("cors");
 app.use(cors());
 
-// Require Mongoose
+// Require mongoose connection and database
 require("./database/mongoose");
+const User = require("./model/user.js");
 
 // Route
-
 app.get("/", (req, res) => {
 	res.send("HELLO WORLD!!!");
+});
+
+// Register
+app.post("/register", async (req, res) => {
+	const user = new User(req.body);
+	try {
+		const cred = await User.findByCredentials(req.body.username, req.body.email);
+		if (cred != "") throw new Error(cred);
+		await user.save();
+		const token = await user.generateAuthToken();
+		res.status(201).send({ user, token });
+	} catch (err) {
+		res.status(400).send({ error: err.message });
+	}
+});
+
+// Login
+app.post("/login", async (req, res) => {
+	try {
+		const user = await User.findByCredentialsForLogin(req.body.email, req.body.password);
+		const token = await user.generateAuthToken();
+		res.send({ token, user });
+	} catch (err) {
+		res.status(400).send({ error: err.message });
+	}
 });
--- model/user.js
+++ model/user.js
@@ -1,16 +1,96 @@
 // Mongoose
 const mongoose = require("mongoose");
 
+// Hash password
+const bcrypt = require("bcryptjs");
+
+// Token
+const jwt = require("jsonwebtoken");
+
 const userSchema = mongoose.Schema(
 	{
-		email: { type: String },
-		password: { type: String },
-		name: { type: String },
-		token: [{ token: { type: String } }],
-		avatar: { type: Buffer },
+		username: {
+			type: String,
+			unique: true,
+			required: true,
+			lowercase: true,
+			minlength: 6,
+		},
+		email: {
+			type: String,
+			unique: true,
+			required: true,
+			lowercase: true,
+		},
+		password: {
+			type: String,
+			required: true,
+			minlength: 7,
+		},
+		tokens: [{ token: { type: String, required: true } }],
+		// avatar: { type: Buffer },
 	},
 	{ timestamps: true }
 );
+
+// Reference Methods
+
+// Generate Token
+userSchema.methods.generateAuthToken = async function () {
+	const user = this;
+	const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+	user.tokens = user.tokens.concat({ token });
+	await user.save();
+	return token;
+};
+
+// Auto hide credentials field in returned data
+userSchema.methods.toJSON = function () {
+	const user = this;
+	const userObject = user.toObject();
+
+	delete userObject.password;
+	delete userObject.tokens;
+
+	return userObject;
+};
+
+// Model methods
+
+// Find duplicate email and used username in register
+userSchema.statics.findByCredentials = async (username, email) => {
+	const user = await User.findOne({ username: username });
+	if (user) {
+		return "Username has already taken!";
+	}
+
+	const mail = await User.findOne({ email });
+	if (mail) return "Email has already registered!";
+
+	return "";
+};
+
+// Find credencials for login
+userSchema.statics.findByCredentialsForLogin = async (email, password) => {
+	const user = await User.findOne({ email });
+	if (!user) throw new Error("Unable to login - Email");
+
+	const isMatch = await bcrypt.compare(password, user.password);
+	if (!isMatch) throw new Error("Unable to login - Password");
+
+	return user;
+};
+
+// Pre and post internal methods
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+	const user = this;
+
+	if (user.isModified("password")) user.password = await bcrypt.hash(user.password, 8);
+
+	next();
+});
 
 const User = mongoose.model("user", userSchema);
 
