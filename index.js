f4734098a8ca0271843a956b3be1c443f62e0d88
thunderphong <thunderthunderphong@gmail.com>
2020-11-09 16:25:31 +0700

    recent

--- index.js
+++ index.js
@@ -33,8 +33,7 @@ app.post("/register", async (req, res) => {
 		const cred = await User.findByCredentials(req.body.username, req.body.email);
 		if (cred != "") throw new Error(cred);
 		await user.save();
-		const token = await user.generateAuthToken();
-		res.status(201).send({ user, token });
+		res.status(201).send({ user });
 	} catch (err) {
 		res.status(400).send({ error: err.message });
 	}
@@ -52,10 +51,18 @@ app.post("/login", async (req, res) => {
 });
 
 // Logout
-app.post("/logout", (req, res) => {
+app.post("/logout", async (req, res) => {
 	try {
-		console.log(req.user);
-		res.send();
+		console.log(req.body);
+		const user = await User.findOne({ username: req.body.username });
+		if (!user) throw new Error("Cannot logout!");
+		else
+			user.tokens = user.tokens.filter((token) => {
+				return token.token !== req.body.token;
+			});
+		// else user.tokens = [];
+		await user.save();
+		res.status(200).send("OK");
 	} catch (err) {
 		res.status(400).send({ error: err.message });
 	}
